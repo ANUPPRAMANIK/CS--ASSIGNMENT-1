6542b37f64573d4f97555ddb63682aa4e80a4ede
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 17:45:13 +0530

    Update question-1.cpp

--- question-1.cpp
+++ question-1.cpp
@@ -2,18 +2,15 @@
 using namespace std;
 main() {
 int a;
-cout<<"enter a number";
+cout<<" enter a number ";
 cin>>a;
-cout<<"your number was:"<<a;
+cout<<" your number was: "<<a;
 float b;
-cout<<"enter your height in meters";
+cout<<" enter your height in meters ";
 cin>>b;
-cout<<"your height is:"<<b;
+cout<<" your height is: "<<b;
 char c[20];
-cout<<"enter your name";
+cout<<" enter your name ";
 cin>>c;
-cout<<"your name is:"<<c;
-bool d;
-cout<<"are you a boy?";
-cin>>d;
+cout<<" your name is: "<<c;
 }
