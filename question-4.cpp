d76a026a3fe7ee7d8e14ebaae3a6eb4e0344d813
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 17:25:52 +0530

    Create question-4.cpp

---
+++ question-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+main(){
+int a,b,c;
+cout<<" enter length ";
+cin>>a;
+cout<<" your length is "<<a;
+cout<<" enter breadth ";
+cin>>b;
+cout<<" your breadth is "<<b;
+c=(2*a) + (2*b);
+cout<<" perimeter: "<<c;
+}
