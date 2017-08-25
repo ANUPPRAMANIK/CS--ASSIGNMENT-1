a501877f0e04fc76744405224566e57c1dc130c6
ANUPPRAMANIK <31176868+ANUPPRAMANIK@users.noreply.github.com>
2017-08-25 17:35:32 +0530

    Create question-6.cpp

---
+++ question-6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+main(){
+float a,b,c,d;
+cout<<" enter radius ";
+cin>>a;
+cout<<" your radius is "<<a;
+b=2*a;
+cout<<" diameter: "<<b;
+c=2*3.14*a;
+cout<<" circumference: "<<c;
+d=3.14*a*a;
+cout<<" area: "<<d;
+}
